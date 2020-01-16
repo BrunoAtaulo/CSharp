2cee3317d11ba4d388bcc2aa05ad4d7a7465e24b
Bruno Ataulo <b.begliomini@gmail.com>
2020-01-16 11:43:59 -0300

    Ex 4 da prova

--- DesafioProvaex4/DesafioProvaex4/Program.cs
+++ DesafioProvaex4/DesafioProvaex4/Program.cs
@@ -12,9 +12,9 @@ namespace DesafioProvaex4
             Supervisor supervisor1 = new Supervisor("Rodrigo", 20, 8000);
             Vendedor vendedor1 = new Vendedor("Luan Everton", 23, 2500);
 
-            Console.WriteLine("O Gerente {0} Mattos, com a idade: {1}, tem o salario R${2:0.00}", gerente1.Nome, gerente1.Idade, gerente1.Salario);
-            Console.WriteLine("O Supervisor {0}, com a idade: {1}, tem o salario R${2:0.00}", supervisor1.Nome, supervisor1.Idade, supervisor1.Salario);
-            Console.WriteLine("O vendedor {0}, com a idade: {1}, tem o salario R${2:0.00}", vendedor1.Nome, vendedor1.Idade, vendedor1.Salario);
+            Console.WriteLine("O Gerente {0} Mattos, com a idade: {1}, tem o salario R${2:0.00}", gerente1.Nome, gerente1.Idade, gerente1.Bonificacao());
+            Console.WriteLine("O Supervisor {0}, com a idade: {1}, tem o salario R${2:0.00}", supervisor1.Nome, supervisor1.Idade, supervisor1.Bonificacao());
+            Console.WriteLine("O vendedor {0}, com a idade: {1}, tem o salario R${2:0.00}", vendedor1.Nome, vendedor1.Idade, vendedor1.Bonificacao());
 
         }
     }
