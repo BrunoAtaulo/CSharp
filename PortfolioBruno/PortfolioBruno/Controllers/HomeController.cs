efca8182f3b85c2c1a7e1e042f5c51fa8a0698c5
Bruno Ataulo <b.begliomini@gmail.com>
2020-01-28 09:55:44 -0300

    Portfolio Bruno

---
+++ PortfolioBruno/PortfolioBruno/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioBruno.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+        public ActionResult Contato()
+        {
+            return View();
+        }
+
+        public ActionResult Projetos()
+        {
+            return View();
+        }
+
+        public ActionResult Skills()
+        {
+            return View();
+        }
+    }
+}
---
+++ PortfolioBruno/PortfolioBruno/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PortfolioBruno.MvcApplication" Language="C#" %>
