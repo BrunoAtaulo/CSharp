e9f49015ff621008b3b88b708ecf3674b0a27f26
Bruno Ataulo <b.begliomini@gmail.com>
2020-02-03 15:05:09 -0300

    Teste MVC com Identity

---
+++ MVC-Identity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_Identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
